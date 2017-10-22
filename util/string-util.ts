e91384eef8500a6bb195bb7bd615e688dad1ebd6
Wang Guan <momocraft@gmail.com>
2017-10-22 14:39:07 +0900

    a random string generator

---
+++ util/string-util.ts
@@ -0,0 +1,25 @@
+export namespace StringUtil {
+
+    const HexPalette
+        = "0123456789ABCDEF";
+
+    const AlphaNumPalette
+        = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    export function randomString(palette: string, length: number) {
+        const chars = new Array(length);
+
+        for (let i = 0; i < 4; i++)
+            chars[i] = palette.charAt(Math.floor(Math.random() * palette.length));
+
+        return chars.join("");
+    }
+
+    export function randomHex(length: number) {
+        return randomString(HexPalette, length);
+    }
+
+    export function randomAlphaNum(length: number) {
+        return randomString(AlphaNumPalette, length);
+    }
+}
