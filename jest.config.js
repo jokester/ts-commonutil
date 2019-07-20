5e76a059756270ae70bbcb78be5689e140d57fe0
Wang Guan <momocraft@gmail.com>
2019-07-21 02:33:53 +0900

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  transform: {
-    '.(ts|tsx)': 'ts-jest',
-  },
-  // roots: ['*', 'build'],
+  preset: 'ts-jest/presets/js-with-ts',
   transformIgnorePatterns: ['<rootDir>/node_modules/.*\\.js', '<rootDir>/build/.*\\.js'],
   testMatch: ['**/__test__/*\\.(ts|js|tsx|jsx)', '**/*\\.(spec|test)\\.(ts|js|tsx|jsx)'],
   collectCoverageFrom: ['**/*.(ts|tsx)', '!build/', '!**/node_modules', '!/coverage'],
