9d26a622d2becb9145481834ffd9e959af250d81
Wang Guan <momocraft@gmail.com>
2019-10-04 23:14:25 +0900

    add DefaultMap

---
+++ collection/default-map.ts
@@ -0,0 +1,12 @@
+export class DefaultMap<K, V> extends Map<K, V> {
+  constructor(private readonly createDefault: (k: K) => V, entries?: readonly ([K, V])[]) {
+    super(entries);
+  }
+
+  getOrCreate(k: K): V {
+    if (!this.has(k)) {
+      this.set(k, this.createDefault(k));
+    }
+    return this.get(k)!;
+  }
+}
