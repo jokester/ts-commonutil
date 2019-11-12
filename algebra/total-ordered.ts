5b262b39876ad53c92fc5c64c0f05c69b4b5cb03
Wang Guan <momocraft@gmail.com>
2019-11-12 23:00:58 +0900

    change NumericOrder / LexicographicalOrder to instance

--- algebra/total-ordered.ts
+++ algebra/total-ordered.ts
@@ -3,8 +3,8 @@ export abstract class TotalOrdered<T> {
 
   abstract equal(a: T, b: T): boolean;
 
-  sort(elems: T[]): T[] {
-    return elems.sort((a, b) => {
+  sort(elements: T[]): T[] {
+    return elements.sort((a, b) => {
       if (this.equal(a, b)) return 0;
 
       const before = this.before(a, b);
@@ -13,11 +13,21 @@ export abstract class TotalOrdered<T> {
   }
 }
 
-export class NumberTotalOrdered extends TotalOrdered<number> {
+export const NumericOrder = new (class extends TotalOrdered<number> {
   before(a: number, b: number): boolean {
     return a < b;
   }
   equal(a: number, b: number): boolean {
     return a === b;
   }
-}
+})();
+
+export const LexicographicalOrder = new (class extends TotalOrdered<string> {
+  before(a: string, b: string): boolean {
+    return a < b;
+  }
+
+  equal(a: string, b: string): boolean {
+    return a === b;
+  }
+})();
