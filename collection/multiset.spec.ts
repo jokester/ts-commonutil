bc63b114a19d77ed667c743c592ae726d19c7bbf
Wang Guan <momocraft@gmail.com>
2020-03-12 00:56:55 +0900

    add multiset

---
+++ collection/multiset.spec.ts
@@ -0,0 +1,20 @@
+import { Multiset } from './multiset';
+
+describe(Multiset, () => {
+  it('keeps elements and count', () => {
+    const testee = new Multiset<string>();
+
+    testee.setCount('abc', 1);
+    testee.setCount('abc', 2);
+
+    expect(testee.getCount('abc')).toEqual(2);
+    expect(testee.getElemsOfCount(1)).toEqual([]);
+    expect(testee.getElemsOfCount(2)).toEqual(['abc']);
+    expect(testee.getElemsOfCount(0)).toEqual([]);
+
+    testee.setCount('abc', 0);
+    expect(testee.maxCount()).toEqual(0);
+    expect(testee.getCount('abc')).toEqual(0);
+    expect(testee.getElemsOfCount(0)).toEqual([]);
+  });
+});
