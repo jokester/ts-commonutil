6cca0987ba90ed4bbf40db0387c880816abce580
Wang Guan <momocraft@gmail.com>
2020-06-16 00:19:22 +0900

    PromiseContainer

---
+++ src/concurrency/promise-container.spec.ts
@@ -0,0 +1,91 @@
+import { PromiseContainer, PromiseState } from './promise-container';
+import { wait } from './timing';
+
+describe('promise-container', () => {
+  it('keeps promised value', async () => {
+    const testee = new PromiseContainer<number>(wait(1e3, 1));
+    expect(testee.state).toEqual(PromiseState.pending);
+
+    await testee;
+    expect(testee.state).toEqual(PromiseState.fulfilled);
+    expect(testee.isFulfilled() && testee.value).toEqual(1);
+  });
+
+  it('only exposes value/reason after type guards', () => {
+    const testee = new PromiseContainer<number>(0);
+
+    if (testee.isFulfilled()) {
+      const shouldBeNum: number = testee.value;
+      // @ts-expect-error
+      const reason: string = testee.reason;
+    }
+
+    if (testee.isRejected()) {
+      // reason is accessible, but unknown
+      const reason = testee.reason as string;
+      // @ts-expect-error
+      const uncastedReason: string = testee.reason;
+    }
+
+    if (testee.isPending()) {
+      // @ts-expect-error
+      testee.reason;
+      // @ts-expect-error
+      testee.value;
+    }
+  });
+
+  it('does not replace a pending promise by default', async complete => {
+    const testee = new PromiseContainer<number>(1);
+
+    const notReplaced = testee.replace(async prev => {
+      complete.fail('should not be called');
+      return NaN;
+    });
+
+    await testee.replace(async prev => 1 + (await prev), { onPending: true });
+
+    expect(await notReplaced).toBe(1);
+    expect(testee.isFulfilled() && testee.value).toBe(2);
+
+    complete();
+  });
+
+  it('does not replace fulfilled promise by default', async complete => {
+    const testee = new PromiseContainer<number>(1);
+    await testee;
+    expect(testee.isFulfilled() && testee.value).toBe(1);
+
+    const notReplaced = await testee.replace(async prev => {
+      complete.fail('should not be called');
+      return NaN;
+    });
+    expect(testee.isFulfilled() && testee.value).toBe(1);
+
+    await testee.replace(async prev => 1 + (await prev), { onFulfilled: true });
+    expect(testee.isFulfilled() && testee.value).toBe(2);
+
+    complete();
+  });
+
+  it('does replace rejected promise by default', async complete => {
+    const testee = new PromiseContainer<number>(Promise.reject('rejected'));
+
+    try {
+      await testee;
+      complete.fail('should throw');
+    } catch {}
+
+    const replaced = await testee.replace(() => 1);
+    expect(replaced).toEqual(1);
+
+    complete();
+  });
+
+  it('DOES cause deadlock when replace() referenced self', async () => {
+    const testee = new PromiseContainer<number>(1);
+
+    const replaced = await testee.replace(async prev => 1 + (await testee) + (await prev), { onPending: true });
+    expect(replaced).toBe(3);
+  });
+});
---
+++ src/concurrency/promise-container.ts
@@ -0,0 +1,112 @@
+export const enum PromiseState {
+  pending = 'pending',
+  fulfilled = 'fulfilled',
+  rejected = 'rejected',
+}
+
+interface ReplaceOptions {
+  onPending?: boolean;
+  onFulfilled?: boolean;
+  onRejected?: boolean;
+}
+
+const empty = {
+  then() {
+    return Promise.reject('empty');
+  },
+} as const;
+
+export class PromiseContainer<T> implements PromiseLike<T> {
+  private innerPromise!: Promise<T>;
+  private _state!: PromiseState;
+
+  private _value: null | T = null;
+  private _reason: unknown = null;
+
+  constructor(initial?: T | PromiseLike<T>, private readonly debug = false) {
+    if (initial) {
+      /* @eslint-disable-next-line */
+      this.replaceInnerPromise(
+        // safe to case: p will be Promise.resolve() anyway
+        initial as PromiseLike<T>,
+      );
+    } else {
+      this._state = PromiseState.rejected;
+      this.innerPromise = (empty as unknown) as Promise<T>; // safe to cast: innerPromise only requires then
+    }
+  }
+
+  get state(): PromiseState {
+    return this._state;
+  }
+
+  isFulfilled(): this is { readonly value: T } {
+    return this._state === PromiseState.fulfilled;
+  }
+
+  isRejected(): this is { readonly reason: unknown } {
+    return this._state === PromiseState.rejected;
+  }
+
+  isPending(): boolean {
+    return this._state === PromiseState.pending;
+  }
+
+  protected get value(): T {
+    return this._value as T;
+  }
+
+  protected get reason(): unknown {
+    return this._reason;
+  }
+
+  /**
+   *
+   * @param {(previous: Promise<T>) => PromiseLike<T>} generator
+   * @param {ReplaceOptions} options
+   * @returns {Promise<T>} a Promise that completes *after*  {@code this} is updated
+   */
+  replace(generator: (previous: PromiseLike<T>) => T | PromiseLike<T>, options: ReplaceOptions = {}): Promise<T> {
+    const { onPending = false, onFulfilled = false, onRejected = true } = options;
+
+    if (
+      (onPending && this._state === PromiseState.pending) ||
+      (onFulfilled && this._state === PromiseState.fulfilled) ||
+      (onRejected && this._state === PromiseState.rejected)
+    ) {
+      return this.replaceInnerPromise(generator(this.innerPromise));
+    }
+    return this.innerPromise;
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?: ((value: T) => PromiseLike<TResult1> | TResult1) | undefined | null,
+    onrejected?: ((reason: any) => PromiseLike<TResult2> | TResult2) | undefined | null,
+  ): Promise<TResult1 | TResult2> {
+    return this.innerPromise.then(onfulfilled, onrejected);
+  }
+
+  private replaceInnerPromise(p: T | PromiseLike<T>): Promise<T> {
+    this._state = PromiseState.pending;
+    this._value = this._reason = null;
+    const newInner = Promise.resolve(p);
+    newInner.then(
+      v => this.onInnerPromiseComplete(newInner, PromiseState.fulfilled, v),
+      e => this.onInnerPromiseComplete(newInner, PromiseState.rejected, e),
+    );
+    return (this.innerPromise = newInner);
+  }
+
+  private onInnerPromiseComplete(innerOnStart: Promise<T>, newState: PromiseState, fulfillOrReject: any) {
+    if (this.innerPromise === innerOnStart) {
+      this._state = newState;
+      if (newState === PromiseState.fulfilled) {
+        this._value = fulfillOrReject;
+        this._reason = null;
+      } else if (newState === PromiseState.rejected) {
+        this._value = null;
+        this._reason = fulfillOrReject;
+      }
+    }
+  }
+}
