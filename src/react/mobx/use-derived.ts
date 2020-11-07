626d112e8c3b98d648f128b286afff2769b8cd28
Wang Guan <momocraft@gmail.com>
2020-11-07 21:31:39 +0900

    add better mobx demo
    upstream:
    https://gist.github.com/jokester/3c842a519ce9f4b02f1a85e7d9fd7332

---
+++ src/react/mobx/use-derived.ts
@@ -0,0 +1,23 @@
+import { autorun, IReactionDisposer } from 'mobx';
+import { useEffect, useRef, useState } from 'react';
+
+export function useDerived<T>(foo: () => T): T {
+  const [count, setCount] = useState(0);
+
+  const valueRef = useRef<T>();
+  const disposeRef = useRef<IReactionDisposer>();
+
+  if (!disposeRef.current) {
+    disposeRef.current = autorun(() => {
+      const prevValue = valueRef.current;
+      const newValue = (valueRef.current = foo());
+      if (/* after first rendering */ disposeRef.current && !Object.is(prevValue, newValue)) {
+        setCount(_ => _ + 1);
+      }
+    });
+  }
+
+  useEffect(() => disposeRef.current, []);
+
+  return valueRef.current!;
+}
