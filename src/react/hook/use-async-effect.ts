09bb9d70ec669d22d338a70a6817c58221b07802
Wang Guan <momocraft@gmail.com>
2023-09-07 00:30:28 +0900

    useAsyncEffect: add preventDuplicatedRun

--- src/react/hook/use-async-effect.ts
+++ src/react/hook/use-async-effect.ts
@@ -1,22 +1,32 @@
 import { RefObject, useEffect, DependencyList } from 'react';
 import { Deferred } from '../../concurrency/deferred';
 
+const nextTick = Promise.resolve();
+
 export function useAsyncEffect(
   effectCallback: (
     running: RefObject<boolean>,
     released: PromiseLike<void>,
   ) => Promise</* cleanup should be done with released */ void>,
   deps?: DependencyList,
+  preventDuplicatedRun = false,
 ): void {
   useEffect(() => {
     const mounted = { current: true };
 
     const effectReleased = new Deferred<void>(true);
 
+    const run = preventDuplicatedRun
+      ? async () => {
+          await nextTick;
+          if (!mounted.current) return;
+          return effectCallback(mounted, effectReleased);
+        }
+      : () => effectCallback(mounted, effectReleased);
     if (typeof setImmediate === 'function') {
-      setImmediate(() => effectCallback(mounted, effectReleased));
+      setImmediate(run);
     } else {
-      setTimeout(() => effectCallback(mounted, effectReleased));
+      setTimeout(run);
     }
 
     return () => {
