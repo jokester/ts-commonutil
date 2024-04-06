9139a3c7457e245faf7e73896a4eb41c495e35ed
Wang Guan <momocraft@gmail.com>
2024-04-06 23:15:45 +0900

    lazyThenable: revise comment & signerature

--- src/concurrency/lazy-thenable.ts
+++ src/concurrency/lazy-thenable.ts
@@ -1,12 +1,13 @@
 /**
- * create a lazy, at-most-once PromiseLike from async function
+ * create a lazy PromiseLike to run {@name io} at most once and only after being awaited
  * @param io
+ * @return
  */
-export function lazyThenable<T>(io: () => PromiseLike<T>): PromiseLike<T> {
-  let r: null | Promise<T> = null;
+export function lazyThenable<T>(io: () => T): PromiseLike<Awaited<T>> {
+  let r: null | Promise<Awaited<T>> = null;
   return {
     then<TResult1, TResult2 = never>(
-      onfulfilled?: ((value: T) => PromiseLike<TResult1> | TResult1) | undefined | null,
+      onfulfilled?: ((value: Awaited<T>) => PromiseLike<TResult1> | TResult1) | undefined | null,
       onrejected?: ((reason: any) => PromiseLike<TResult2> | TResult2) | undefined | null,
     ): PromiseLike<TResult1 | TResult2> {
       return (r ??= Promise.resolve(io())).then(onfulfilled, onrejected);
