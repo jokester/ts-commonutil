f77d2bacb2c17202a9b33c14247e797f00f1df1d
Wang Guan <momocraft@gmail.com>
2024-04-20 23:01:27 +0900

    fix

---
+++ src/stress/sortBy.spec.ts
@@ -0,0 +1,12 @@
+import { sortBy } from './sortBy';
+
+describe('orderBy', () => {
+  it('sorts value DESC by "natural" JS ordering', () => {
+    expect(sortBy([2, 3, 1], (v) => v, false)).toEqual([3, 2, 1]);
+    expect(sortBy([1, 1, 2], (v) => v, false)).toEqual([2, 1, 1]);
+  });
+  it('sorts value ASC by "natural" JS ordering', () => {
+    expect(sortBy([2, 3, 1], (v) => v)).toEqual([1, 2, 3]);
+    expect(sortBy([1, 1, 2], (v) => v)).toEqual([1, 1, 2]);
+  });
+});
