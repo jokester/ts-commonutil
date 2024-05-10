d1bfc2787666509e0f009e67520bba736c33c99e
Wang Guan <momocraft@gmail.com>
2024-05-11 01:58:06 +0900

    test

--- src/concurrency/lazy-thenable.spec.ts
+++ src/concurrency/lazy-thenable.spec.ts
@@ -7,10 +7,13 @@ describe(lazyThenable, () => {
     const lazy1 = lazyThenable(async () => ++called);
 
     expect(called).toEqual(0);
+    expect(lazy1.executed).toBe(false)
 
     const converted = Promise.resolve(lazy1);
     expect(called).toEqual(0);
+    expect(lazy1.executed).toBe(true)
     await wait(0);
+    expect(lazy1.executed).toBe(true)
     expect(called).toEqual(1);
     expect(await converted).toEqual(1);
     expect(await lazy1).toEqual(1);
