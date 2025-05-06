827c3b904dd656418354f2e95d55e310cb5c4e26
Wang Guan <momocraft@gmail.com>
2025-05-06 19:19:36 +0900

    update

--- src/stress/sample.ts
+++ src/stress/sample.ts
@@ -5,7 +5,7 @@ export function sample<T>(input: readonly T[], rng = defaultRng): T {
   return input[index];
 }
 
-export function sampleSize<T>(from: T[], count: number, rng = defaultRng): T[] {
+export function sampleSize<T>(from: readonly T[], count: number, rng = defaultRng): T[] {
   const reservoir: T[] = from.slice(0, Math.min(count, from.length));
 
   for (let i = count; i < from.length; i++) {
